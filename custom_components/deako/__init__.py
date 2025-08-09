42c829689911927d1c70a197736d361117eef8f5
Joseph Gruber <joseph.gruber@gmail.com>
2025-08-09 13:20:48 -0600

    Remediates the deprecated async_forward_entry_setup method

--- custom_components/deako/__init__.py
+++ custom_components/deako/__init__.py
@@ -46,7 +46,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
+                hass.config_entries.async_forward_entry_setups(entry, platform)
             )
 
     entry.add_update_listener(async_reload_entry)
